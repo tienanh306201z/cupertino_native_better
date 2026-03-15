d551d2c362cd469524ad301ea734be37b73a0c75
Alva Tran <ceilingprogressproductions2001@gmail.com>
2026-03-16 05:32:52 +0700

    change podfile target

--- example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/emirgunumdogdu/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/emirgunumdogdu/development/cupertino_native_better/example"
+export "FLUTTER_ROOT=/Users/user/fvm/versions/3.41.2"
+export "FLUTTER_APPLICATION_PATH=/Users/user/Projects/MobileProjects/cupertino_native_better/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
